4230ba57b0e0ff49fb2e7d98a199d965d303c05b
Esa Niemi <esa.niemi@protonmail.com>
2022-06-15 18:47:45 +0300

    Added loading test data from server

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+from datetime import datetime, timedelta
+from random import random
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 _data = {
-    'humidity': [1, 2, 3, 4, 5, 6]
+    'humidity': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 10 + 20 for x in range(10)]
+    },
+    'temperature': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 20 + 10 for x in range(10)]
+    },
+    'co2': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 1100 + 400 for x in range(10)]
+    },
+    'tvoc': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 50for x in range(10)]
+    },
+    'pm25': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 50 for x in range(10)]
+    },
+    'pm10': {
+        'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
+        'y': [random() * 25 for x in range(10)]
+    },
 }
 
 
