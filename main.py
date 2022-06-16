bc8f541a2b1389a9fdf44491ed65673ec529c175
Esa Niemi <esa.niemi@protonmail.com>
2022-06-16 12:53:27 +0300

    Added connection to Cosmos DB

--- main.py
+++ main.py
@@ -1,96 +1,63 @@
-from datetime import datetime, timedelta
-from random import random
+import uuid
+from datetime import datetime
+from azure.cosmos import CosmosClient, PartitionKey
 
 from flask import Flask, request, render_template, Response
 
-app = Flask(__name__)
 
-# _data = {
-#     'temperature': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 10 + 20 for x in range(10)]
-#     },
-#     'humidity': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 20 + 10 for x in range(10)]
-#     },
-#     'co2': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 1100 + 400 for x in range(10)]
-#     },
-#     'tvoc': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 50 for x in range(10)]
-#     },
-#     'pm25': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 50 for x in range(10)]
-#     },
-#     'pm10': {
-#         'x': [(datetime.now() + timedelta(hours=x)).isoformat() for x in range(10)],
-#         'y': [random() * 25 for x in range(10)]
-#     },
-# }
+def init_routes(app, cosmos_container):
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-_data = {
-    'temperature': {
-        'x': [],
-        'y': []
-    },
-    'humidity': {
-        'x': [],
-        'y': []
-    },
-    'co2': {
-        'x': [],
-        'y': []
-    },
-    'tvoc': {
-        'x': [],
-        'y': []
-    },
-    'pm25': {
-        'x': [],
-        'y': []
-    },
-    'pm10': {
-        'x': [],
-        'y': []
-    },
-}
+    @app.route("/api", methods=['GET'])
+    def get_data():
+        query = 'SELECT TOP 60 c.co2, c.humidity, c.temperature, c.pm25, c.pm10, c.tvoc, c.timestamp FROM c ORDER BY c.timestamp'
+        items = list(cosmos_container.query_items(
+            query=query,
+            enable_cross_partition_query=True
+        ))
+        print(items)
+        return {'data': items}
+
+    @app.route("/api", methods=['POST'])
+    def append_data():
+        print(request.json)
+        timestamp = datetime.now().isoformat()
+        _data = {
+            'id': str(uuid.uuid4()),
+            'timestamp': timestamp,
+            **request.json
+        }
+        print(_data)
+
+        cosmos_container.create_item(_data)
+
+        return Response(status=200)
+
+    return app
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def init_cosmos(client):
+    database_name = 'airquality-database'
+    database = client.create_database_if_not_exists(id=database_name)
 
+    container_name = 'airquality-container'
+    container = database.create_container_if_not_exists(
+        id=container_name,
+        partition_key=PartitionKey(path="/timestamp"),
+        offer_throughput=400
+    )
+    return container
 
-@app.route("/api", methods=['GET'])
-def get_data():
-    return _data
-
-
-@app.route("/api", methods=['POST'])
-def append_data():
-    print(request.json)
-    timestamp = datetime.now().isoformat()
-    _data['temperature']['y'].append(request.json['temperature'])
-    _data['temperature']['x'].append(timestamp)
-    _data['humidity']['y'].append(request.json['humidity'])
-    _data['humidity']['x'].append(timestamp)
-    _data['co2']['y'].append(request.json['co2'])
-    _data['co2']['x'].append(timestamp)
-    _data['tvoc']['y'].append(request.json['tvoc'])
-    _data['tvoc']['x'].append(timestamp)
-    _data['pm25']['y'].append(request.json['pm25'])
-    _data['pm25']['x'].append(timestamp)
-    _data['pm10']['y'].append(request.json['pm10'])
-    _data['pm10']['x'].append(timestamp)
-    print(_data)
-    return Response(status=200)
 
 def main():
-    app.run(host='localhost')
+    app = Flask(__name__)
+    cosmos_client = CosmosClient('https://localhost:8081',
+                                 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==')
+    cosmos_container = init_cosmos(cosmos_client)
+    app = init_routes(app, cosmos_container)
+    app.run(host='0.0.0.0')
 
 
 if __name__ == '__main__':
